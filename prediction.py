bb108d9b636305041b4363e5fc886e4593ebe20a
iyungalam <115508333+iyungalam@users.noreply.github.com>
2026-07-21 15:59:15 +0700

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,17 +1,26 @@
+from pathlib import Path
 import joblib
 
-model = joblib.load("model/gboost_model.joblib")
-result_target = joblib.load("model/encoder_target.joblib")
+# Menentukan lokasi folder project
+BASE_DIR = Path(__file__).resolve().parent
+MODEL_DIR = BASE_DIR / "model"
+
+# Load model dan encoder
+model = joblib.load(MODEL_DIR / "gboost_model.joblib")
+result_target = joblib.load(MODEL_DIR / "encoder_target.joblib")
+
 
 def prediction(data):
     """Making prediction
 
     Args:
-        data (Pandas DataFrame): Dataframe that contain all the preprocessed data
+        data (Pandas DataFrame): Dataframe that contains all the data
+        needed to make prediction.
 
     Returns:
         str: Prediction result (Good, Standard, or Poor)
     """
     result = model.predict(data)
     final_result = result_target.inverse_transform(result)[0]
+
     return final_result
